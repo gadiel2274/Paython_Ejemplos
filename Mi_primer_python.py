141c295739d1118128ae38227d991938730329da
gadiel2274 <gadiel2274@gmail.com>
2025-03-11 10:57:56 +0100

    V1.0.6

--- Mi_primer_python.py
+++ Mi_primer_python.py
@@ -6,6 +6,7 @@ comentarios multilineas"""
 #cuarto comentario
 #quinto comentario
 # comentario añadido desde github para hacer git pull
+#Comentario añadido desde la RamaTemporal, no se vera desde la master
 
 
 
